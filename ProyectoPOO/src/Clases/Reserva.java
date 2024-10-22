a375adf6b1ca8ea1ca334dff13a84ffd9bf4efc6
GeraldineLoli <gery.loli0105@gmail.com>
2024-10-22 17:38:40 -0500

    Creé la clase Reserva. - Loli

---
+++ ProyectoPOO/src/Clases/Reserva.java
@@ -0,0 +1,63 @@
+
+package Clases;
+
+public class Reserva {
+    private String numeroReserva;
+    private Cliente cliente;
+    private Habitacion habitacion;
+    private String fechaInicio;
+    private String fechaFin;
+
+    public Reserva() {
+    }
+
+    public Reserva(String numeroReserva, Cliente cliente, Habitacion habitacion, String fechaInicio, String fechaFin) {
+        this.numeroReserva = numeroReserva;
+        this.cliente = cliente;
+        this.habitacion = habitacion;
+        this.fechaInicio = fechaInicio;
+        this.fechaFin = fechaFin;
+    }
+
+    public String getNumeroReserva() {
+        return numeroReserva;
+    }
+
+    public void setNumeroReserva(String numeroReserva) {
+        this.numeroReserva = numeroReserva;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Habitacion getHabitacion() {
+        return habitacion;
+    }
+
+    public void setHabitacion(Habitacion habitacion) {
+        this.habitacion = habitacion;
+    }
+
+    public String getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(String fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public String getFechaFin() {
+        return fechaFin;
+    }
+
+    public void setFechaFin(String fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+    
+    
+}
