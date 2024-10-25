4c3dd9032d548f125c81234a02c0f220b68523dc
LAB-USR-CHIMBOTE <LAB-USR-CHIMBOTE@grupoutp.net>
2024-10-25 16:37:49 -0500

    Actualicé los cambios de la clase HabitacionList. -Loli

--- ProyectoPOO/src/ArrayList/HabitacionList.java
+++ ProyectoPOO/src/ArrayList/HabitacionList.java
@@ -1,13 +1,26 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package ArrayList;
 
-/**
- *
- * @author LAB-USR-CHIMBOTE
- */
+import Clases.Habitacion;
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class HabitacionList {
+    List<Habitacion> listaHabitaciones = new ArrayList<>();
     
+    public void agregar(Habitacion h){
+        listaHabitaciones.add(h);
+    }
+    
+    public void quitar(Habitacion h){
+        listaHabitaciones.remove(h);
+    }
+    
+    public Habitacion obtener(int pos){
+        return listaHabitaciones.get(pos);
+    }
+    public List<Habitacion> listar(){
+        return this.listaHabitaciones;
+    }
 }
