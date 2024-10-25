f8f34ee223abce13c598237a50d583a91e732f5f
LAB-USR-CHIMBOTE <LAB-USR-CHIMBOTE@grupoutp.net>
2024-10-25 16:55:15 -0500

    Actualicé la lista de cliente. -Chumpitaz

--- ProyectoPOO/src/ArrayList/ClienteList.java
+++ ProyectoPOO/src/ArrayList/ClienteList.java
@@ -1,5 +1,25 @@
 package ArrayList;
 
+import Clases.Cliente;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ClienteList {
-    List <ClienteList>
+    List <Cliente> listaCliente = new ArrayList<>();
+    
+    public void agregar(Cliente C){
+        listaCliente.add(C);
+    }
+    
+     public void quitar(Cliente C){
+        listaCliente.remove(C);
+    }
+     
+      public Cliente obtener(int pos){
+        return listaCliente.get(pos);
+    }
+      
+      public List<Cliente> listar(){
+          return this.listaCliente;
+      }
 }
